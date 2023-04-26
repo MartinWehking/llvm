91869a6252226827fae51d0f7ada9677824a626d
Martin <martin.wehking@codeplay.com>
2023-04-26 16:57:35 +0100

    Fix copy creation

--- sycl/plugins/cuda/pi_cuda.cpp
+++ sycl/plugins/cuda/pi_cuda.cpp
@@ -4037,6 +4037,7 @@ pi_result cuda_piEventRelease(pi_event event) {
         assert(event->get_queue() != nullptr);
 
         auto temp_queue = event->get_queue();
+        auto copy = std::make_unique<_pi_event>(std::move(*event_ptr));
         temp_queue->cached_events.emplace(std::move(event_ptr));
         result = PI_SUCCESS;
       }
